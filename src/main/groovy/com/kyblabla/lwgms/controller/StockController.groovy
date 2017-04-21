0a649ae880c5a708cd3f05dd9d86ac90f5a76d50
kyyblabla <kyyblabla@163.com>
2017-04-21 18:44:28 +0800

    加入几个小接口

--- src/main/groovy/com/kyblabla/lwgms/controller/StockController.groovy
+++ src/main/groovy/com/kyblabla/lwgms/controller/StockController.groovy
@@ -1,6 +1,12 @@
 package com.kyblabla.lwgms.controller
 
+import com.kyblabla.lwgms.model.Stock
+import com.kyblabla.lwgms.service.StockService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,9 +17,22 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class StockController {
 
-    @GetMapping("/hello")
-    String hello() {
-        return "hello"
+    @Autowired
+    StockService stockService
+
+    @GetMapping("/{id}")
+    Stock getById(@PathVariable("id") int id) {
+        stockService.getById(id)
+    }
+
+    @PostMapping()
+    Stock addStock(@RequestBody Stock stock) {
+        stockService.addStock(stock)
+    }
+
+    @PostMapping("/delivery")
+    Stock deliveryAssign(@RequestBody Stock stock) {
+        stockService.addStock(stock)
     }
 
 }
